d77064387f8350f2f8283ee0932de97756aadf97
Senal Kumarage <senalkumarage@gmail.com>
2019-08-15 17:56:11 +0800

    Commented few files

--- server/src/api/routes/image.js
+++ server/src/api/routes/image.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
 const route = express.Router();
+
+/**
+ * This module is responsible for all the routes related to images
+ */
 module.exports = (app) => {
 
     console.log('starting image api');
 
     app.use('/public-feed', route);
 
-    app.get('/public-feed', (req,res) => {
+    app.get('/public-feed', (req, res) => {
         res.send('Hello world');
     })
 }
