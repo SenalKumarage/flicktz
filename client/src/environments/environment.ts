266c1eda15641593bac7f5adceb72ccaf766f3d8
Senal Kumarage <senalkumarage@gmail.com>
2019-08-19 07:50:30 +0800

    Enabling the prod mode

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: 'http://ec2-18-218-129-235.us-east-2.compute.amazonaws.com/'
 };
 
